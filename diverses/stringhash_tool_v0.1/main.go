a9eb193bd325ff46994cd7b30a31c63f69a6366d
Rasputin <axxit666@gmx.de>
2017-10-21 07:11:55 +0200

    Überarbeitung

--- diverses/stringhash_tool_v0.1/main.go
+++ diverses/stringhash_tool_v0.1/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/jzelinskie/whirlpool"
 	"os"
+	"encoding/hex"
 )
 
 func main(){
@@ -19,4 +20,15 @@ func main(){
 	w.Write(hash)
 	fmt.Println("Argument: ", *stringoption)
 	fmt.Printf("Hash: %x\n",  w.Sum(nil))
+	// check
+	fmt.Println("Check")
+	hashstring := "b913d5bbb8e461c2c5961cbe0edcdadfd29f068225ceb37da6defcf89849368f8c6c2eb6a4c4ac75775d032a0ecfdfe8550573062b653fe92fc7b8fb3b7be8d6"
+	checkstring := hex.EncodeToString(w.Sum(nil))
+	fmt.Println(checkstring)
+	if checkstring == hashstring {
+		fmt.Println("Hash passt")
+	} else {
+		fmt.Println("etwas passt nicht")
+		os.Exit(1)
+	}
 }
