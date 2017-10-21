09010c7c27421e8fa32fe4dcb31cd3e76cf76daa
Rasputin <axxit666@gmx.de>
2017-10-21 06:31:37 +0200

    Überarbeitung

--- diverses/stringhash_tool_v0.1/main.go
+++ diverses/stringhash_tool_v0.1/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main(){
-	
+	stringoption := flag.String("s", "", "an string, -s=\"test test2\"")
+	flag.Parse()
+	fmt.Println(*stringoption)
 }
