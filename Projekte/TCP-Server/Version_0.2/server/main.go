70d6591546e4f553df68846edc1b44fee8431658
Rasputin <axxit666@gmx.de>
2017-10-22 11:22:57 +0200

    Überarbeitung

--- Projekte/TCP-Server/Version_0.2/server/main.go
+++ Projekte/TCP-Server/Version_0.2/server/main.go
@@ -13,13 +13,13 @@ func main ()  {
 	// wir lauschen auf allen interfaces auf Port 9000
 	ln, err := net.Listen("tcp", ":9000")
 	if err != nil {
-		fmt.Println("Der konnte nicht gestartet werden:", err)
+		fmt.Println("Der konnte nicht gestartet werden:", err.Error())
 		os.Exit(1)
 	}
 	// akzeptieren der Verbindungen zum Port
 	conn, err := ln.Accept()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
 	}
 	fmt.Println("Es wurde eine Verbindung hergestellt ")
 	// eine unendliche Schleife (Abbruch mit Strg + c)
@@ -27,7 +27,7 @@ func main ()  {
 		// warten auf ende der Übertragung mit newline (\n)
 		message, err := bufio.NewReader(conn).ReadString('\n')
 		if err != nil {
-			fmt.Println("Es trat ein Fehler bei der Verbindung auf:", err)
+			fmt.Println("Es trat ein Fehler bei der Verbindung auf:", err.Error())
 		}
 		// Ausgabe der Übertragenen Nachricht
 		fmt.Println("nachricht erhalten:", string(message))
