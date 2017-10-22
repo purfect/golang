c4d87bc5f082aebe06078e55707231a3674bbc29
Rasputin <axxit666@gmx.de>
2017-10-22 06:41:43 +0200

    Überarbeitung

--- Projekte/Systeminfo/main.go
+++ Projekte/Systeminfo/main.go
@@ -10,12 +10,15 @@ import (
 func main() {
 	mem, _ := mem.VirtualMemory()
 	host, _ := host2.Info()
-	fmt.Println("Arch: ", runtime.GOARCH)
-	fmt.Println("OS: ", runtime.GOOS)
-	fmt.Println("Kernel: ", host.KernelVersion)
-	fmt.Println("CPUs:i ", runtime.NumCPU())
+	fmt.Println("Arch:", runtime.GOARCH)
+	fmt.Println("OS:", runtime.GOOS)
+	fmt.Println("Platform:", host.Platform, host.PlatformVersion)
+	fmt.Println("Kernel:", host.KernelVersion)
+	fmt.Println("CPUs:", runtime.NumCPU())
 	fmt.Printf("RAM (Used): %f%%\n", mem.UsedPercent)
-	fmt.Println("Hostname: ", host.Hostname)
-	fmt.Println("Uptime: ", host.Uptime/60, "Minuten (", (host.Uptime/60)/60, "h)")
-	fmt.Println("Host-ID: ", host.HostID)
+	fmt.Println("Hostname:", host.Hostname)
+	fmt.Println("Uptime:", host.Uptime/60, "Minuten (", (host.Uptime/60)/60, "h)")
+	fmt.Println("Host-ID:", host.HostID)
+	fmt.Println("VirtualizationRole:", host.VirtualizationRole)
+	
 }
--- Projekte/TCP-Server/Version_0.1/server/main.go
+++ Projekte/TCP-Server/Version_0.1/server/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"io"
-	"net"
 	"time"
+	"net"
 )
 
 func main() {
