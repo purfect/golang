fcd3f8646ac0c198059708ed6361e889d2afe4ac
Rasputin <axxit666@gmx.de>
2017-10-26 09:59:24 +0200

    Überarbeitung

--- channel/01/main.go
+++ channel/01/main.go
@@ -8,15 +8,24 @@ import (
 const count = 10
 
 func main() {
+	// Die Channels
 	even := make(chan int)
 	odd := make(chan int)
+	//quit wird zum beenden der Schleife beim empfangen der Werte aus den Channeln benötigt
 	quit := make(chan bool)
+	// Funktion zum Senden der zufallsnummern in den Channel
 	go send(even, odd, quit)
+	// Funktion zum Empfangen der zufallsnummern aus den Channeln
 	receive(even, odd, quit)
 }
 func send(even, odd chan<- int, quit chan<- bool) {
+	// erzeugen der Zufallsnummern
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
+	// Zufälliger Integer zwischen 0 - 100 wird bei jedem Aufruf in der Schleife erzeugt
+	// und geprüft ob dieser durch 2 teilbar ist
+	// gerade Werte gehen in den even-channel
+	// ungerade Werte gehen in den odd-channel
 	for i := 0; i < count; i++ {
 		checkint := r1.Intn(100)
 		if checkint%2 == 0 {
@@ -27,13 +36,14 @@ func send(even, odd chan<- int, quit chan<- bool) {
 	}
 	quit <- true
 }
+//
 func receive(even, odd <-chan int, quit <-chan bool) {
 	for {
 		select {
 		case v := <-even:
-			fmt.Println("gerade: ",v)
+			fmt.Printf("gerade:\t\t%d\n",v)
 		case v := <-odd:
-			fmt.Println("ungerade: ", v)
+			fmt.Printf("ungerade:\t%d\n", v)
 		case <-quit:
 			return
 		}
