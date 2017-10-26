35c482908d91692fda50e5aa73ef6c390190bea1
Rasputin <axxit666@gmx.de>
2017-10-26 09:40:12 +0200

    Überarbeitung

--- channel/01/main.go
+++ channel/01/main.go
@@ -7,10 +7,38 @@ import (
 )
 
 func main() {
+
+	even := make(chan int)
+	odd := make(chan int)
+	quit := make(chan bool)
+	go send(even, odd, quit)
+	receive(even, odd, quit)
+}
+func send(even, odd chan<- int, quit chan<- bool) {
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
+	//count := 10
 	for i := 0; i < 10; i++ {
-		fmt.Print("Random-Number: ")
-		fmt.Println(r1.Intn(100))
+		checkint := r1.Intn(100)
+		if checkint%2 == 0 {
+			even <- checkint
+		} else {
+			odd <- checkint
+		}
+	}
+	quit <- true
+	close(even)
+	close(odd)
+}
+func receive(even, odd <-chan int, quit <-chan bool) {
+	for {
+		select {
+		case v := <-even:
+			fmt.Println("even-chan: ",v)
+		case v := <-odd:
+			fmt.Println("odd-chan: ", v)
+		case <-quit:
+			return
+		}
 	}
 }
