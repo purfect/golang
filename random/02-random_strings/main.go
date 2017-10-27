42a9e908f12af88f86067f1445c465875345bae1
Rasputin <axxit666@gmx.de>
2017-10-27 11:59:36 +0200

    Überarbeitung

---
+++ random/02-random_strings/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	r1 := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := range b {
+		b[i] = letters[r1.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func main() {
+	fmt.Println(randSeq(10))
+}
