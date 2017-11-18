4194b9d3fb80f27c3b538d612953332bb331408a
rasputin <rasputin@localhost.localdomain>
2017-11-18 11:47:10 +0100

    Update

--- misc/sqlite/00/main.go
+++ misc/sqlite/00/main.go
@@ -8,17 +8,18 @@ import (
 )
 
 func main() {
+    var id int
+    var username string
+    var alias string
+
     database, _ := sql.Open("sqlite3", "./foo.db")
     statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, username TEXT, alias TEXT)")
     statement.Exec()
     statement, _ = database.Prepare("INSERT INTO people (username, alias) VALUES (?, ?)")
     statement.Exec("rasputin", "=^_^=")
     rows, _ := database.Query("SELECT id, username, alias FROM people")
-    var id int
-    var firstname string
-    var lastname string
-    for rows.Next() {
-        rows.Scan(&id, &firstname, &lastname)
-        fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+   for rows.Next() {
+        rows.Scan(&id, &username, &alias)
+        fmt.Println(strconv.Itoa(id) + ": " + username + " " + alias)
     }
 }
