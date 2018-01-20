c935499ba3b1d74c1fce11e2d9dc59e1dac718ce
Rasputin <axxit666@gmx.de>
2018-01-20 18:56:42 +0100

    Überarbeitung

---
+++ misc/http/01/client/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	bs, err := ioutil.ReadAll(conn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(bs))
+}
